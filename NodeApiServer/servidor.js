bf6649907d37811726b3174586edb2857d3339f9
jrafa.ramon <jrrn@MacBook-Pro-de-JRRN.local>
2017-07-16 12:47:23 +0200

    Post Meth od Lista {text}

--- NodeApiServer/servidor.js
+++ NodeApiServer/servidor.js
@@ -3,19 +3,40 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost:27017/ApiAngularWithNode');
+mongoose.createConnection('mongodb://localhost:27017/ApiAngularWithNode');
 
 var Lista = mongoose.model('Lista', {
-    texto: String
+    text: String
 });
 
-app.configure(function() {
+app.get(function() {
     app.use(express.static('env' + '/public'));
+    app.use(express.bodyParser());
+})
+
+app.post('/api/lista', function(request, response) {
+    Lista.create({
+        text: request.body.texto
+    }, function(error, lista) {
+        if (error) {
+            response.send(error)
+        }
+        Lista.find(function(error, lista) {
+            if (error) {
+                response.send(error);
+            }
+            response.json(lista);
+        })
+    })
+
+
 })
 
 app.get('*', function(request, resource) {
     resource.sendfile('./public/index.html')
 });
+
+
 
 app.listen(8080, function() {
     console.log('Node Api For Angular 2 in localhost:8080')
